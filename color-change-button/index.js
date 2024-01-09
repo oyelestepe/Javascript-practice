e3344332cd1ebcd58f02a76976e6be2e2916f508
Onurcan Yelestepe <oyelestepe1@gmail.com>
2024-01-09 19:06:23 +0300

    Color change buttons

---
+++ color-change-button/index.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('.body');
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const green = document.querySelector('.green');
+const orange = document.querySelector('.orange');
+const purple = document.querySelector('.purple');
+
+red.addEventListener('click', () => {
+body.style.backgroundColor = '#B31312'
+});
+
+
+blue.addEventListener('click', () => {
+body.style.backgroundColor = '#0174BE'
+});
+
+green.addEventListener('click', () => {
+body.style.backgroundColor = '#004225'
+});
+
+
+orange.addEventListener('click', () => {
+body.style.backgroundColor = '#FF5B22'
+});
+
+purple.addEventListener('click', () => {
+body.style.backgroundColor = '#363062'
+});
