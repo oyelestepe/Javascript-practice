78ff8a9cee1d681a5c2dbb7e0047c2d105e629c5
Onurcan Yelestepe <oyelestepe1@gmail.com>
2024-03-27 20:51:31 +0300

    feat: Comment section added

--- social-media/app.js
+++ social-media/app.js
@@ -1,7 +1,38 @@
 const likeBtn = document.querySelector('.like-button');
 const likeIcon = document.querySelector('#like-icon');
 
-
+// like button
 likeBtn.addEventListener( 'click', () => {
     likeIcon.classList.toggle("like-icon-active")
 });
+
+
+// add comment
+
+const commentInput = document.getElementById('comment-input');
+const commentUl = document.getElementById('comment-ul');
+const commentLi = document.getElementById('comment-li');
+
+function comment() {
+    let input = commentInput.value
+    let createComment = document.createElement('li');
+    let commentText = createComment.innerText = input;
+
+    console.log(input)
+   if(input === '') {
+        alert("Enter some text ")
+    } else {
+        document.getElementById('comment-ul').appendChild(createComment).classList.add('comment-li');
+        let deleteComment = document.createElement("button");
+        deleteComment.innerHTML = "delete"
+        createComment.appendChild(deleteComment).setAttribute('id', 'close');
+    };
+
+    commentInput.value = "";    
+
+    commentUl.addEventListener('click', (e) => {
+        if(e.target.id === 'close'){
+            e.target.parentElement.remove()
+        }
+    })
+}
