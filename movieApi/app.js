c859d0e0a51de029630b31bdfa652f1d79dbc010
Onurcan Yelestepe <oyelestepe1@gmail.com>
2024-04-10 19:42:21 +0300

    perf: Data received from the API was integrated into the html file

--- movieApi/app.js
+++ movieApi/app.js
@@ -1,7 +1,6 @@
 const input = document.getElementById('input');
 const btn = document.getElementById('search-btn')
-const container = document.getElementsByClassName('container')
-
+const container = document.querySelector('.container')
 const title = document.getElementById('title');
 const genre = document.getElementById('genre');
 const actors = document.getElementById('actors');
@@ -18,32 +17,33 @@ btn.addEventListener('click', getData)
 
 
 async function getData(){
-    let apiURL = `http://www.omdbapi.com/?t=${input.value}&apikey=${key}`
-    let data = await fetch(apiURL);
-    let response = await data.json();
+    let movieName = input.value
+    let apiURL = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`
+   
     
-    title.innerText = response.Title
-    genre.innerText = response.Genre
-    actors.innerText = response.Actors
-    rating.innerText = response.imdbRating
-    director.innerText = response.Director
-    plot.innerText = response.Plot 
-    runtime.innerText = response.Runtime
-    type.innerText = response.Type 
-    rated.innerText = response.Rated 
-    poster.src = response.Poster
-    /* <h1 id="title">${response.Title}</h1> 
-    <h1 id="title">${response.Title}</h1>
-    <h3 id="genre">${response.Genre}</h3>
-    <h4 id="actors">${response.Actors}</h4>
-    <h3 id="rating">${response.imdbRating}</h3>
-    <h3 id="director">${response.Director}</h3>
-    <p id="plot">${response.Plot}</p>
-    <h5 id="runtime">${response.Runtime}</h5>
-    <h4 id="type">${response.Type}</h4>
-    <h5 id="rated">${response.Rated}</h5>*/
-
+    if(movieName.length <= 0){
+        container.innerHTML = "<h3>Please enter a movie name</h3>";
+    } else {
+        let data = await fetch(apiURL);
+        let response = await data.json();
+        if(response.Response == "True"){
+            container.innerHTML = `
+            <div class="img-container">
+                <img src="${response.Poster}" id="poster-img">
+            </div>
+            <h1 id="title">${response.Title}</h1> 
+            <h1 id="title">${response.Title}</h1>
+            <h3 id="genre">${response.Genre}</h3>
+            <h4 id="actors">${response.Actors}</h4>
+            <h3 id="rating">${response.imdbRating}</h3>
+            <h3 id="director">${response.Director}</h3>
+            <p id="plot">${response.Plot}</p>
+            <h5 id="runtime">${response.Runtime}</h5>
+            <h4 id="type">${response.Type}</h4>
+            <h5 id="rated">${response.Rated}</h5>
+            `
+        } else {
+            container.innerHTML = `<h3>${response.Error} </h3>`
+        }
+    }
 }
-
-getData()
-
