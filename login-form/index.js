fb5eb693130c608dbfbf7fc9ae96ba2919394fc4
Onurcan Yelestepe <oyelestepe1@gmail.com>
2024-04-16 21:36:56 +0300

    wrong password section

--- login-form/index.js
+++ login-form/index.js
@@ -1,14 +1,26 @@
 const usernameInput = document.getElementById('usernameInput');
 const passwordInput = document.getElementById('passwordInput');
-const btn = document.getElementById('btn');
+const btn = document.querySelector('.btn');
 const info = document.getElementById('info');
 
 btn.addEventListener('click', () => {
     if(usernameInput.value == 'user' && passwordInput.value == 'admin'){
        info.innerText = 'Logged in successfully'
     } else {
-        info.innerText = 'Access denied'
+        addClass()
+        setTimeout(() => {
+            removeClass()
+        },10000)       
     }
     
 })
 
+function addClass(){
+    info.innerText = 'Access denied, try again in 10 seconds'
+    btn.classList.add("disabled")
+}
+
+function removeClass(){
+    info.innerText = 'Try again'
+    btn.classList.remove("disabled")
+}
