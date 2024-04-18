0e1536e3cd77b817dd4ab1bb86a18212f6024e4d
Onurcan Yelestepe <oyelestepe1@gmail.com>
2024-04-18 17:20:08 +0300

    Transfer project skeleton to javascript

--- weather app/index.js
+++ weather app/index.js
@@ -4,20 +4,23 @@ const weather = document.getElementById('weather');
 const weather_description = document.getElementById('weather-description');
 const cityName = document.getElementById('cityName');
 const img = document.getElementById('img');
-
+const container = document.querySelector('.info')
 async function fetchData() {
     try{
         const city = document.getElementById('cityInput').value;
         const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         const data = await response.json()
-        cityName.innerText = data.name
-        weather.innerText = data.weather[0].main
-        weather_description.innerText = data.weather[0].description
         const celcius = data.main.temp - 273
-        temp.innerText = Math.round(celcius) +"°c"
         const imgIcon = data.weather[0].icon;
-        img.src = `https://openweathermap.org/img/wn/${imgIcon}@2x.png`
-        img.style.display = "block"
+        const imgLink = `https://openweathermap.org/img/wn/${imgIcon}@2x.png`
+
+        container.innerHTML = `
+        <h1 id="cityName">${data.name}</h1>
+        <img src="${imgLink}" id="img">
+        <h1 id="temp">${Math.round(celcius) +"°c"}</h1>
+        <h1 id="weather">${data.weather[0].main}</h1>
+        <h2 id="weather-description">${data.weather[0].description}</h2>
+        `
     }
     catch (error) {
         console.error(error);
